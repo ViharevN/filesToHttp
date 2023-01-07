513013880bdf0e60880e1fb744f96e5f5e94211a
ViharevN <swix_off@mail.ru>
2023-01-07 23:21:01 +0300

    commit

---
+++ src/main/java/me/viharev/filestohttp/FilestohttpApplication.java
@@ -0,0 +1,13 @@
+package me.viharev.filestohttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestohttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilestohttpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/viharev/filestohttp/FilestohttpApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.filestohttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestohttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
